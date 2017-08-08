47b8d174efeb8f41f9ba95a4a3d8e703d96ee4d3
Arandi Lopez <arandilopez.93@gmail.com>
2017-08-07 22:48:55 -0500

    vuetify installed

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Vuetify from 'vuetify'
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   routes: [
