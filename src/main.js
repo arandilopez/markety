8f5b5b34b42b7fa2a4a9575ffd7cfdb487afdaff
Arandi Lopez <arandilopez.93@gmail.com>
2017-08-12 23:04:14 -0500

    vuetify declaracion should be in main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ShoppingList from '@/components/ShoppingList'
-import Vuetify from 'vuetify'
 
 Vue.use(Router)
-Vue.use(Vuetify)
 
 export default new Router({
   routes: [
