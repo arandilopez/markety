0ec8b0c8ac766520dd238d0792d79494573fb721
Arandi Lopez <arandilopez.93@gmail.com>
2017-08-13 10:54:50 -0500

    lorem ipsun about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,18 @@
 <template lang="html">
-
+<div class="about">
+  <v-card style="margin-bottom: 48px;">
+    <v-card-text>
+      <h3>About Markety</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h4>License</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </v-card-text>
+  </v-card>
+</div>
 </template>
 
 <script>
