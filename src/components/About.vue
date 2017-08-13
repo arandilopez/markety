bda5d95cc7b361f916d252d51fc049601c6e2e99
Arandi Lopez <arandilopez.93@gmail.com>
2017-08-13 13:29:34 -0500

    demo about

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,10 +5,17 @@
       <h3>About Markety</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <h4>License</h4>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Markety is licensed under MIT License {{year}}
+      </p>
+      <p class="text-xs-center">
+        <v-icon class="black--text">code</v-icon> with
+        <v-icon class="pink--text">favorite</v-icon>
+        by <a href="https://arandilopez.me">Arandi López</a>
       </p>
     </v-card-text>
   </v-card>
@@ -17,6 +24,11 @@
 
 <script>
 export default {
+  computed: {
+    year () {
+      return new Date().getFullYear()
+    }
+  }
 }
 </script>
 
